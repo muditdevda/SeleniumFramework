434c501eb6e1b76c592b77defd870253d5b83508
Nikhlesh <nikhleshkumargujrati01@gmail.com>
2023-08-09 20:09:30 +0530

    first commit

--- src/main/java/Utility/DriverManager.java
+++ src/main/java/Utility/DriverManager.java
@@ -3,41 +3,51 @@ package Utility;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import ru.stqa.selenium.factory.WebDriverPool;
 
 public class DriverManager {
 	private static WebDriver driver;
-//	private static PropertiesHelper ph = new PropertiesHelper();
-	private static final String DRIVER_PATH = "src/resources/driver/win";
+	private static PropertiesHelper ph = new PropertiesHelper();
+	private static final String DRIVER_PATH = "src/resources/chromedriver_win32";
 	
 	public static WebDriver getChromeDriver() {
-		
-		if (new PropertiesHelper().getBrowserName()=="chrome") {
-			String fulldriverPath = null;
+		if (ph.getBrowserName().equals("chrome")) {
+			String fulldriverPath = "C:\\Users\\HP\\Downloads\\chromedriver_win32\\chromedriver.exe";
 			if(System.getProperty("os.name").equalsIgnoreCase("linux")) {
 				fulldriverPath = DRIVER_PATH + "linux/chromedriver.exe";
 			}
 			else if (System.getProperty("os.name").equalsIgnoreCase("windows")){
-				fulldriverPath = DRIVER_PATH + "win/chromedriver.exe";
+				fulldriverPath = DRIVER_PATH + "chromedriver_win32/chromedriver.exe";
 			}
 			System.setProperty("webdriver.chrome.driver", fulldriverPath);
 		}
 		return getDriverFromPool(getChromeOptions());
 	}
 	
-	private static WebDriver getDriverFromPool(MutableCapabilities chromeOptions) {
+	
+	
+	public static WebDriver getDriverFromPool(MutableCapabilities chromeOptions) {
 		driver = WebDriverPool.DEFAULT.getDriver(chromeOptions);
 		return driver;
 	}
+	
+	public static void Url() {
+		driver.get("https://amazon.in");
+	}
 
-	private static ChromeOptions getChromeOptions() {
+	public static ChromeOptions getChromeOptions() {
 		ChromeOptions options = new ChromeOptions();
 		options.setPageLoadStrategy(PageLoadStrategy.NONE);
+		options.addArguments("--remote-allow-origins=*");
 		options.addArguments("--start-maximized");
 		options.addArguments("--disable-extensions");
 		options.addArguments("--disable-gpu");
@@ -45,8 +55,7 @@ public class DriverManager {
 		prefrence.put("download.default_directory", "/home/selenium/Downloads");
 		prefrence.put("download.prompt_for_download", false);
 		prefrence.put("safebrowsing.enabled", false);
-		options.setExperimentalOption("prefs", prefrence);
-		
+		options.setExperimentalOption("prefs", prefrence);	
 		return options;
 		
 	}
--- src/test/java/DevelopedFramework/developedframework/testClass.java
+++ src/test/java/DevelopedFramework/developedframework/testClass.java
@@ -1,23 +1,35 @@
 package DevelopedFramework.developedframework;
-
-
+import java.io.File;
+import java.io.IOException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
+import com.google.common.io.Files;
 import Utility.DriverManager;
-import Utility.PropertiesHelper;
 
 public class testClass {
-	
-	@BeforeTest
+	Logger log = LogManager.getLogger(testClass.class);
+	private static WebDriver driver;
+	@BeforeTest 
 	public void setup() {
 		System.out.println("print");
-		DriverManager.getChromeDriver();
+		driver = DriverManager.getChromeDriver();
+		DriverManager.Url();
+		log.info("Browser Start Successfully");
+		log.debug("for debug");
+		log.error("error message");
+		log.warn("warning message");
 	}
 
 	@Test
-	public void firstTest() {
-		
+	public void firstTest() throws IOException {
+		File f = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		Files.copy(f,new File("D:\\NIkhlesh_Ruby\\SeleniumFramework\\src\\ScreensShotImage\\amazonscreenshot.jpg"));
+		log.info("Browser Stop Successfully");
 	}
 	
 
